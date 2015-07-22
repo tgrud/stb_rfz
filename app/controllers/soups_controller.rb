f846a1a1f2abd14b5f6412e0a8933db638ed32b1
tgrud <tgrud@users.noreply.github.com>
2015-07-22 23:06:54 +0000

    Add JSON view rendering for an individual soup

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -5,5 +5,9 @@ class SoupsController < ApplicationController
     
     def show
         @soup = Soup.find(params[:id])
+        respond_to do |format|
+            format.html
+            format.json { render json: @soup }
+        end
     end
 end
