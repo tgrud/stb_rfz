fd48172343ca234ca9bdd78e94b18ac659b25965
tgrud <tgrud@users.noreply.github.com>
2015-07-22 23:52:05 +0000

    Dry up soups controller with before action. Add resource actions to controller

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,4 +1,6 @@
 class SoupsController < ApplicationController
+    before_action :fetch_soup, only: [:show, :edit, :update, :destroy]
+    
     def index
         @soup = Soup.all
         respond_to do |format|
@@ -8,10 +10,32 @@ class SoupsController < ApplicationController
     end
     
     def show
-        @soup = Soup.find(params[:id])
         respond_to do |format|
             format.html
             format.json { render json: @soup }
         end
     end
+    
+    def new
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+    
+    end
+    
+    def destroy
+    
+    end
+    
+    private
+    
+    def fetch_soup
+        @soup = Soup.find(params[:id])
+    end
+    
 end
