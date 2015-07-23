73be782f4d6dd2493bc01e705439fd2aaeec3865
tgrud <tgrud@users.noreply.github.com>
2015-07-23 02:34:57 +0000

    Session hack to show a secret soup

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -38,6 +38,11 @@ class SoupsController < ApplicationController
         redirect_to @soup
     end
     
+    def secret_soups
+        session[:vip] = true
+        redirect_to root_path
+    end
+    
     private
     
     def fetch_soup
